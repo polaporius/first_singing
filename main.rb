9bcd33e0b80e9ffd600e77a43103edbfde5b6558
Menduabev Rustam <nneowoolf@gmail.com>
2013-10-28 14:04:10 +0700

    init commit

---
+++ main.rb
@@ -0,0 +1,67 @@
+require 'sinatra'
+require 'pony'
+
+
+
+ 
+get '/' do
+  erb :index
+end
+
+
+get '/bio' do
+  erb :bio
+end
+
+
+get '/hobby' do
+  erb :hobby
+end
+
+
+
+get '/msg' do
+  
+
+  erb :msg
+end
+
+
+
+post '/msg' do
+
+  options = 
+  {
+    :to => params[:email],
+    :from => 'nneowoolf@gmail.com',
+    :subject => 'Test',
+    :body => 'Test Text',
+    :html_body => params[:text],
+    :via => :smtp,
+    :via_options => 
+      {
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :enable_starttls_auto => true,
+      :user_name => 'nneowoolf@gmail.com',
+      :password => 'fgj23hrt',
+      :authentication => :plain,
+      :domain => 'localhost'
+      }
+  }
+  @result = "Хер знает что"
+  Pony.mail(options)
+
+  #redirect '/sended'
+  end
+
+
+get '/sended' do
+  erb :sended
+end
+
+#get '/msg' do
+
+ # erb :msg
+#end
+
