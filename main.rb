486e826cf75f611de66f1d0e20a118872818c06c
Menduabev Rustam <nneowoolf@gmail.com>
2013-10-31 00:00:03 +0700

    Updated the code

--- main.rb
+++ main.rb
@@ -25,23 +25,22 @@ end
 
 post '/msg' do
   @errors << 'Email is not valid'  unless params[:email].match EMAIL_REGEX
-  @errors << 'Text must have 1 letter at least' unless params[:text].empty?
+  @errors << 'Text must have 1 letter at least' if params[:text].empty?
   unless @errors.any?
     options = 
     {
       :to => 'nneowoolf@gmail.com',
       :from => params[:email],
       :subject => 'Test',
-      :body => 'Test Text',
-      :html_body => @params[:text],
+      :body => "from: #{params[:email]}#{params[:text]}",
       :via => :smtp,
       :via_options => 
         {
         :address => 'smtp.gmail.com',
         :port => 587,
         :enable_starttls_auto => true,
-        :user_name => 'nneowoolf@gmail.com',
-        :password => 'fgj23hrt23rust',
+        :user_name => 'put your login',
+        :password => 'put your password',
         :authentication => :plain,
         :domain => 'localhost'
         }
